c05fd46ff0769946b6987f4d3780bc493528b2aa
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2025-03-18 23:22:58 -0700

    Update YT.js

--- api/YT.js
+++ api/YT.js
@@ -6,7 +6,7 @@ const meta = {
     description: "Fetch YouTube video information and download links",
     author: "LocDev",
     method: "get",
-    category: "social",
+    category: "Downloader",
     path: "/youtube?url="
 };
 
